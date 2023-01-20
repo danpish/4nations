5e77969f0ced030f38b19efd2736b26ce4b72bcb
danpish <94285332+danpish@users.noreply.github.com>
2023-01-20 20:37:57 +0330

    Detecting mouse collision
    base work for selecting cards

--- main.py
+++ main.py
@@ -7,16 +7,34 @@ with a terrible mental health tooo
 from ursina import *
 dock = []
 
+g_mouseX = 0
+g_mouseY = 0
+pov_mouseX = 0
+pov_mouseY = 0
+hitting = 255
+	
+
 
 a = 0
 def update():
-	global a
+	global a, g_mouseY, g_mouse, pov_mouseX, pov_mouseY, hitting
 	camera.position = (0,0,a)
 	#a += 0.1
-
+	#print("mousex " + str(mouse.x) + ": mousey" + str(mouse.y))#for mouse positioning debug
+	#lets cast a beam from the mosue
+	mouse_origin = Vec3(0,0,0)
+	cast_direction = (mouse.x + mouse.x / 8, mouse.y + mouse.y / 8,2) #It was not supposed to work with this but somehow it did
+	mouse_raycast = raycast(mouse_origin, cast_direction, debug=True)
+	if mouse_raycast.hit:
+		hitting = mouse_raycast.entity.idi
+	else:
+		hitting = 255
+	
+	
+	
 textures = ["Iran", "france", "armen", "argen"]
 
-class cards(Entity):# I;m just praying that this works //LATER // OH GOODNESS IT WORKES
+class cards(Entity):# I;m just praying that this works //LATER// OH GOODNESS IT WORKES
 	def __init__(self, position, ido):
 		global textures
 		super().__init__(
@@ -36,11 +54,10 @@ class cards(Entity):# I;m just praying that this works //LATER // OH GOODNESS IT
 		self.goup = False
 		self.godown = True
 	def update(self):
-		if self.goup:
+		global hitting
+		if hitting == self.idi:
 			if self.position.y < 0.5:
 				self.position = (self.position.x,self.position.y + 0.01,self.position.z)
-			else:
-				self.goup = False
 		#if self.godown:
 		else:
 			if self.position.y >= 0.0:
@@ -60,12 +77,7 @@ dock.append(cards((-2,0,10), 0))
 dock.append(cards((-1,0,10), 1))
 dock.append(cards((1,0,10), 2))
 dock.append(cards((2,0,10), 3))
-"""
-dock[0].rotation = (0,0,-60)
-dock[1].rotation = (0,0,-30)
-dock[2].rotation = (0,0,30)
-dock[3].rotation = (0,0,60)
-"""
+#add rotation here if needed
 dock[1].picked_up()
 #Windos = Text(text="Windwos XP", position=(0,1,10))
 #GOD DAMN WHAT THE FU
