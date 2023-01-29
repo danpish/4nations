92f36a5f00568a8d2188fd96ef7ffd4e181f3169
danpish <94285332+danpish@users.noreply.github.com>
2023-01-29 23:29:27 +0330

    changed card handeling + card clicking test
    phew ... that took more time than it should

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ with an engine that i dont really know and a mountain of exams on the horizon
 with a terrible mental health tooo 
 """
 from ursina import *
-dock = []
+dock = ["","","","",""]
 hitting = 255
 a = 0
+was_mouse_down = True
 def ofset(value):
 	return (value - 1.6)**3 * (1/8) + 0.5
 def update():
@@ -18,8 +19,7 @@ def update():
 	#lets cast a beam from the mosue
 	mouse_origin = Vec3(0,0,0,)
 	mouseRad  = Vec3(math.radians(mouse.x), math.radians(mouse.y),0)
-	#cast_direction = (mouse.x + mouse.x / 8, mouse.y + mouse.y / 8,2) #It was not supposed to work with this but somehow it did
-	cast_direction = (mouseRad.x, mouseRad.y,0.0285)#done?
+	cast_direction = (mouseRad.x, mouseRad.y,0.0285)#yes its done
 	mouse_raycast = raycast(mouse_origin, cast_direction, debug=True)
 	if mouse_raycast.hit:
 		hitting = mouse_raycast.entity.idi
@@ -29,7 +29,7 @@ def update():
 textures = ["Iran", "france", "armen", "argen"]
 
 class cards(Entity):# I;m just praying that this works //LATER// OH GOODNESS IT WORKES
-	def __init__(self, position, ido, xpos):
+	def __init__(self, position, ido, xpos, slot_position):
 		global textures
 		super().__init__(
 			parent = scene,
@@ -37,10 +37,9 @@ class cards(Entity):# I;m just praying that this works //LATER// OH GOODNESS IT 
 			model="quad",
 			collider = "cube",
 			idi = ido,
-			goup = False,
-			godown = False,
 			texture = textures[ido],
-			xpos = xpos
+			xpos = xpos,
+			slot_position = slot_position
 		)
 	"""
 	def picked_up(self):
@@ -51,22 +50,43 @@ class cards(Entity):# I;m just praying that this works //LATER// OH GOODNESS IT 
 		self.godown = True
 	"""
 	def update(self):
-		global hitting
-		movement_speed = 2 * time.dt
+		global hitting, mouse_down, was_mouse_down
+		movement_speed = 4 * time.dt
 		if hitting == self.idi:
 			if self.xpos < 1.0:
 				self.position = (self.position.x,ofset(self.xpos),self.position.z)
 				self.xpos += movement_speed
-		#if self.godown:
 		else:
 			if self.xpos > 0.0:
 				self.position = (self.position.x,ofset(self.xpos),self.position.z)
 				self.xpos -= movement_speed
-			#else:
-			#	self.godown = False
 		if self.xpos < 0.0:#Fail safe
 			self.xpos = 0.0
 
+		if hitting == self.idi:
+			if mouse.left:
+				mouse_down = True
+				was_mouse_down = False
+			else:
+				mouse_down = False
+			if not mouse_down and not was_mouse_down:
+				was_mouse_down = True
+				do_delete = False
+				if self.position.x == -2 and dock[0] != "":
+					dock[0] = ""
+					do_delete = True
+				elif self.position.x == -1 and dock[1] != "":
+					dock[1] = ""
+					do_delete = True
+				elif self.position.x == 1 and dock[2] != "":
+					dock[2] = ""
+					do_delete = True
+				elif self.position.x == 2 and dock[3] != "":
+					dock[3] = ""
+					do_delete = True
+				print(dock)
+				if do_delete:
+					self.disable()
 game = Ursina()
 window.isfullscreen = False
 window.borderless = False
@@ -75,12 +95,11 @@ window.exit_button.enabled = False
 window.fps_counter.visible = True
 #Cube = Entity(model = "quad", position = (0,-1,10), texture='Iran')
 #                     color = color.blue
-dock.append(cards((-2,0,10), 0,0))
-dock.append(cards((-1,0,10), 1,0))
-dock.append(cards((1,0,10), 2,0))
-dock.append(cards((2,0,10), 3,0))
+dock[0]=(cards((-2,0,10), 0,0,0))
+dock[1]=(cards((-1,0,10), 1,0,1))
+dock[2]=(cards((1,0,10), 2,0,2))
+dock[3]=(cards((2,0,10), 3,0,3))
 #add rotation here if needed
-#Windos = Text(text="Windwos XP", position=(0,1,10))
 table = Entity(model="cube", position=Vec3(0,-4.5,10), scale=8, color = color.brown)
 Sky()
 game.run()
