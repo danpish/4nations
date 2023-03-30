cd0de159147cb39e73850d62eb927c87ad2c1d47
danpish <danialpishi@gmail.com>
2023-03-30 15:20:58 +0430

    Update main.py
    changed networking functions to class for clarity. However STILL CAN'T GET SOCK.SETBLOCKING(0) TO WORK!!!!

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ from ursina import *
 import socket
 import pickle
 import threading
-current_dock = [0,0,0,0,0]
-recived_dock = [0,0,0,0,0]
+import time
+current_dock = [1,2,3,0,0]
+recived_dock = [1,2,3,0,0]
 was_mouse_down = True
 send_card = False
 connected_ID = 255
-
+testing = False
 
 def card_movement_diagram(value):
 	return (value - 1.6)**3 * (1/8) + 0.5
@@ -22,94 +23,95 @@ def update():
 
 textures = ["ir", "fr", "am", "ar"]
 
-def setup_server_client():
-	global client, server_port , server_address
-	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)#TCP connection(hopefully)
-	server_port = 8008
-	server_address = "127.0.0.1"#Later to be replaced with user given address
+class scoket_client():
+    def __init__(self, sock=None, addr = "127.0.0.1", port = 8008):
+        if sock == None:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        else:
+            self.sock = sock
+        #self.sock.setblocking(0) GOD HELP
+        self.addr = "127.0.0.1"
+        self.port = 8008
 
-def server_connect():
-	client.connect((server_address, server_port))
-	#Clean code! Am I doing this correctly?
+    def connect(self):
+        self.sock.connect((self.addr, self.port))
+        #Clean code! Am I doing this correctly?
 
-def send_data(data):
-	send_data = data.encode()
-	client.send(send_data)
+    def send_data(self, data):
+        to_send_data = data.encode()
+        self.sock.send(to_send_data)
 
-def recive_data():
-	data = b""
-	while True:
-		part = client.recv(1024)
-		data += part
-		if len(part) < 1024:
-			#Copied from stackoverflow, I don't undrestand this
-			break
-	return data
-
-
+    def recive_data(self):
+        data = self.sock.recv(8126)
+        return data
 
 class cards(Entity):
-	def __init__(self, position, ido, xpos, slot_position):
-		global textures
-		super().__init__(
-			parent = scene,
-			position = position,
-			model="sphere",
-			collider = "sphere",
-			idi = ido,
-			texture = textures[ido],
-			xpos = xpos,
-			slot_position = slot_position
-		)
-	def update(self):
-		global is_mouse_down, was_mouse_down, send_card
+    def __init__(self, position, ido, xpos, slot_position):
+        global textures
+        super().__init__(
+            parent = scene,
+            position = position,
+            model="sphere",
+            collider = "sphere",
+            idi = ido,
+            texture = textures[ido],
+            xpos = xpos,
+            slot_position = slot_position
+        )
+    def update(self):
+        global is_mouse_down, was_mouse_down, send_card
 
-		if held_keys['r']:
-			self.xpos = 0
+        if held_keys['r']:
+            self.xpos = 0
+        movement_speed = 4 * time.dt
+        self.rotation = Vec3(0,self.rotation.y + (movement_speed * 16),0)
+        if self.hovered:
+            if self.xpos < 1.0:
+                self.xpos += movement_speed
+        else:
+            if self.xpos > 0.0:
+                self.xpos -= movement_speed
+        if self.xpos < 0.0:#Fail safe
+            self.xpos = 0.0
+        elif self.xpos > 5.0:
+            self.xpos = 5.0
+        self.position = (self.position.x,card_movement_diagram(self.xpos),self.position.z)
+        if self.hovered:
+            if mouse.left:
+                #I don't know if Ursina has it predefined but here it goes, self made click function
+                is_mouse_down = True
+                was_mouse_down = False
+            else:
+                is_mouse_down = False
+            if not is_mouse_down and not was_mouse_down:
+                was_mouse_down = True
+                do_delete = False
+                if self.position.x == -1.5 and current_dock[0] != "":
+                    current_dock[0] = ""
+                    do_delete = True
+                elif self.position.x == -0.5 and current_dock[1] != "":
+                    current_dock[1] = ""
+                    do_delete = True
+                elif self.position.x == 0.5 and current_dock[2] != "":
+                    current_dock[2] = ""
+                    do_delete = True
+                elif self.position.x == 1.5 and current_dock[3] != "":
+                    current_dock[3] = ""
+                    do_delete = True
+                if not testing:
+                    if do_delete and send_card:
+                        client.send_data(self.idi)
+                        msg = pickle.loads(recive_data())
+                        if msg:
+                            if msg[0] == 22:
+                                send_card = False
+                                self.disable()
+                else:
+                    self.disable()
 
-		movement_speed = 4 * time.dt
-		self.rotation = Vec3(0,self.rotation.y + (movement_speed * 16),0)
-		if self.hovered:
-			if self.xpos < 1.0:
-				self.xpos += movement_speed
-		else:
-			if self.xpos > 0.0:
-				self.xpos -= movement_speed
-		if self.xpos < 0.0:#Fail safe
-			self.xpos = 0.0
-		elif self.xpos > 5.0:
-			self.xpos = 5.0
-		self.position = (self.position.x,card_movement_diagram(self.xpos),self.position.z)
-		if self.hovered:
-			if mouse.left:
-				#I don't know if Ursina has it predefined but here it goes, self made click function
-				is_mouse_down = True
-				was_mouse_down = False
-			else:
-				is_mouse_down = False
-			if not is_mouse_down and not was_mouse_down:
-				was_mouse_down = True
-				do_delete = False
-				if self.position.x == -1.5 and current_dock[0] != "":
-					current_dock[0] = ""
-					do_delete = True
-				elif self.position.x == -0.5 and current_dock[1] != "":
-					current_dock[1] = ""
-					do_delete = True
-				elif self.position.x == 0.5 and current_dock[2] != "":
-					current_dock[2] = ""
-					do_delete = True
-				elif self.position.x == 1.5 and current_dock[3] != "":
-					current_dock[3] = ""
-					do_delete = True
-				if do_delete and send_card:
-					client.send(self.idi)
-					msg = pickle.loads(recive_data())
-					if msg:
-						if msg[0] == 22:
-							send_card = False
-							self.disable()
 game = Ursina()
+
+client = scoket_client()
 
 def on_begin():
 	global recived_dock
@@ -141,21 +143,29 @@ def show_main_menu():
     Main_menu_exit.enabled = True
 
 def join_function():
-	setup_server_client()
-	server_connect()
-	x = threading.Thread(target=multiplayer_thread, args=())
-	x.start()
-	on_begin()
+    client.connect()
+    x = threading.Thread(target=multiplayer_thread, args=())
+    x.start()
+    on_begin()
 
+def test_function():
+    x = threading.Thread(target=single_player_test, args=())
+    x.start()
+    on_begin()
+
+def single_player_test():
+    global testing, send_card
+    testing = True
+    send_card = True
 
 def multiplayer_thread():
-    global send_card, recived_dock
+    global send_card, recived_dock, testing
+    testing = False
     while True:
         print("im still here")
         try:
             print("before data recive")
-            client.setblocking(0)
-            data = recive_data()
+            data = client.recive_data()
             print("after data revcive")
         except socket.error as e:
             print(e)
@@ -202,7 +212,8 @@ exit_text  = Text(parent=Main_menu_exit, text = "EXIt", position=(-0.15, 0.1, 0)
 
 Main_menu_exit.on_click = application.quit
 Main_menu_start.on_click = on_start
-Main_menu_join.on_click = lambda:on_begin([0,1,2,3])
+#Main_menu_join.on_click = lambda:on_begin([0,1,2,3])
+Main_menu_join.on_click = test_function
 Main_menu_host.on_click = join_function
 Main_menu_go_back.on_click = show_main_menu
 Sky()
