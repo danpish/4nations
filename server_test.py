d3935167463bd946d5489d9068cb47913651735d
danpish <danialpishi@gmail.com>
2023-04-14 15:22:15 +0430

    Fixed not sending data to all clients
    Plus the todo list. From past works

--- server_test.py
+++ server_test.py
@@ -7,8 +7,8 @@ server_address = ""#automatic? maybe?
 server_port = 8008
 server.bind((server_address, server_port))
 server.listen()
-conn = None
-addr = None
+conn = []
+addr = []
 g_data = None
 """
 	country codes
@@ -30,62 +30,53 @@ Net functions / data[0]
 """
 
 def join_all_players():
-	global players,player_list,conn,addr,all_players_in
-	while not all_players_in:
-		conn, addr = server.accept()
-		print("got connection from " + str(addr))
-		for x in range(len(player_list)):
-			if not player_list[x]:
-				player_list[x] = True
-				conn.send(pickle.dumps([1,players[x], x]))
-				break
-		if player_list[0] and player_list[1] and player_list[2] and player_list[3]:
-			print("we are all in")
-			all_players_in = True
+    global players,player_list,conn,addr,all_players_in
+    while not all_players_in:
+        c_conn, c_addr = server.accept()
+        print("got connection from " + str(c_addr))
+        for x in range(len(player_list)):
+            if not player_list[x]:
+                player_list[x] = True
+                c_conn.send(pickle.dumps([1,players[x], x]))
+                conn.append(c_conn)
+                addr.append(c_addr)
+                break
+        print(len(conn))
+        if len(conn) >= 4:
+            print("we are all in")
+            all_players_in = True
 
 def reciver():#kind of pathetic. but you got to do what you got to do
-	global g_data
-	l_data = conn.recv(1024)
-	g_data = pickle.loads(l_data)
+    global g_data,conn
+    for x in range(len(conn)):
+        l_data = conn[x].recv(1024)
+    if l_data:
+        g_data = pickle.loads(l_data)
 
-"""
-global players, player_list, conn, addr, g_data
-		current_player = 0
-	x = threading.Thread(target=reciver, args=())
-	x.start()
-	did_player_answer = False
-	while True:
-		did_player_answer = False
-		print("now asking player " + str(current_player))
-		while not did_player_answer:
-			conn.send(pickle.dumps([2,current_player])
-					  if g_data:
-						  if g_data[1] == current_player:
-					players[x].remove(g_data[1])
-					conn.send(pickle.dumps([22]))
-					did_player_answer = True
-					current_player += 1
-					if current_player > 4:
-						current_player = 0
-"""
+def send_to_all(message):
+    global conn
+    for x in range(len(conn)):
+        conn[x].send(message)  
+
 def play():
-	global players, player_list, conn, addr, g_data
-	current_player = 0
-	x = threading.Thread(target=reciver, args=())
-	x.start()
-	did_player_answer = False
-	while True:
-		did_player_answer = False
-		print("now asking pkayer " + f"{current_player}")
-		while not did_player_answer:
-			conn.send(pickle.dumps([2,current_player]))
-			if g_data:
-				if g_data[1] == current_player:
-					players[current_player].remove(g_data[0])
-					conn.send(pickle.dumps([22]))
-		current_player += 1
-		if current_player > 4:
-			current_player -= 4
+    global players, player_list, conn, addr, g_data
+    current_player = 0
+    x = threading.Thread(target=reciver, args=())
+    x.start()
+    did_player_answer = False
+    while True:
+        did_player_answer = False
+        print("now asking pkayer " + f"{current_player}")
+        while not did_player_answer:
+            send_to_all(pickle.dumps([2,current_player]))
+            #conn.send(pickle.dumps([2,current_player]))
+            if g_data:
+                if g_data[1] == current_player:
+                    players[current_player].remove(g_data[0])
+                    conn[current_player].send(pickle.dumps([22]))
+        current_player += 1
+        if current_player > 4:
+            current_player -= 4
 
 def main():
 	global players, player_list
